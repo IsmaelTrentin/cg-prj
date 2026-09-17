1534a28d4488a12dc8c153bb254a536253be53b3
i <ismael.trentin@student.supsi.ch>
2026-09-17 18:29:04 +0200

    feat: add spdlog

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -1,8 +1,8 @@
 /**
- * @file		main.cpp
- * @brief	Client application (that uses the graphics engine)
+ * @file        main.cpp
+ * @brief   Client application (that uses the graphics engine)
  *
- * @author	Achille Peternier (C) SUPSI [achille.peternier@supsi.ch] <<
+ * @author  Achille Peternier (C) SUPSI [achille.peternier@supsi.ch] <<
  * change this to your group members
  */
 
@@ -55,6 +55,8 @@ int main(int argc, char *argv[]) {
 #include <GL/freeglut.h>
 #endif
 
+#include "spdlog/spdlog.h"
+
 #include <FreeImage.h>
 #include <glm/glm.hpp>
 
@@ -72,6 +74,12 @@ void display() {
 }
 
 int main(int argc, char** argv) {
+    spdlog::info("hello spdlog");
+    spdlog::error("Some error message with arg: {}", 1);
+    spdlog::warn("Easy padding in numbers like {:08d}", 12);
+    spdlog::critical("Support for int: {0:d};  hex: {0:x};  oct: {0:o}; bin: {0:b}", 42);
+    spdlog::info("Support for floats {:03.2f}", 1.23456);
+
     // Credits:
     std::cout << "Client application example, A. Peternier (C) SUPSI" << std::endl;
     std::cout << std::endl;
