551ce0e6404d4c730a1d635b5508134788dd7166
i <ismael.trentin@student.supsi.ch>
2026-09-20 14:48:44 +0200

    fix: better test example

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -48,7 +48,21 @@ int main(int argc, char *argv[]) {
 }
 */
 
-// Minimal OpenGL + freeglut + FreeImage test: draws a FreeImage-made texture.
+#define GL_SILENCE_DEPRECATION // macOS: hide OpenGL deprecation warnings
+#include <GL/freeglut.h>
+#ifdef __APPLE__
+#include <OpenGL/gl.h>
+#else
+#include <GL/gl.h>
+#endif
+#include <FreeImage.h>
+#include <cstdio>
+#include <spdlog/spdlog.h>
+
+// Minimal freeglut + OpenGL + FreeImage test.
+// Draws the classic RGB gradient triangle, plus a small checkerboard quad
+// whose texture is generated in memory with FreeImage (no files needed).
+
 #ifndef GL_BGRA // Windows gl.h is GL 1.1 only
 #define GL_BGRA 0x80E1
 #endif
@@ -59,31 +73,68 @@ int main(int argc, char *argv[]) {
 #include <windows.h>
 #endif
 
-#define GL_SILENCE_DEPRECATION // macOS: hide OpenGL deprecation warnings
-#include <GL/freeglut.h>
-#ifdef __APPLE__
-#include <OpenGL/gl.h>
-#else
-#include <GL/gl.h>
-#endif
-#include <FreeImage.h>
+static GLuint tex = 0;
 
-#include <cstdio>
-#include <spdlog/spdlog.h>
+static void makeTexture() {
+    // 64x64 32-bit bitmap (BGRA), red/blue checkerboard
+    FIBITMAP* img = FreeImage_Allocate(64, 64, 32);
+    for (unsigned y = 0; y < 64; ++y)
+        for (unsigned x = 0; x < 64; ++x) {
+            BYTE v = ((x / 8 + y / 8) % 2) ? 255 : 40;
+            RGBQUAD c = {v, 0, (BYTE)(255 - v), 255}; // B, G, R, A
+            FreeImage_SetPixelColor(img, x, y, &c);
+        }
+
+    glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+    glGenTextures(1, &tex);
+    glBindTexture(GL_TEXTURE_2D, tex);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, 64, 64, 0, GL_BGRA, GL_UNSIGNED_BYTE,
+                 FreeImage_GetBits(img));
+    FreeImage_Unload(img);
+}
 
 static void display() {
     glClear(GL_COLOR_BUFFER_BIT);
+
+    // Gradient triangle (red, green, blue corners)
+    glDisable(GL_TEXTURE_2D);
+    glBegin(GL_TRIANGLES);
+    glColor3f(1, 0, 0);
+    glVertex2f(-0.6f, -0.6f);
+    glColor3f(0, 1, 0);
+    glVertex2f(0.6f, -0.6f);
+    glColor3f(0, 0, 1);
+    glVertex2f(0.0f, 0.7f);
+    glEnd();
+
+    // Small textured quad in the bottom-left corner (FreeImage texture)
+    glEnable(GL_TEXTURE_2D);
+    glColor3f(1, 1, 1);
     glBegin(GL_QUADS);
     glTexCoord2f(0, 0);
-    glVertex2f(-0.8f, -0.8f);
+    glVertex2f(-0.95f, -0.95f);
     glTexCoord2f(1, 0);
-    glVertex2f(0.8f, -0.8f);
+    glVertex2f(-0.65f, -0.95f);
     glTexCoord2f(1, 1);
-    glVertex2f(0.8f, 0.8f);
+    glVertex2f(-0.65f, -0.65f);
     glTexCoord2f(0, 1);
-    glVertex2f(-0.8f, 0.8f);
+    glVertex2f(-0.95f, -0.65f);
     glEnd();
+
+    GLenum err = glGetError();
+    if (err != GL_NO_ERROR)
+        std::printf("GL error: 0x%x\n", err);
+
     glutSwapBuffers();
+}
+
+static void reshape(int w, int h) {
+    glViewport(0, 0, w, h);
+}
+static void idle() {
+    glutPostRedisplay();
 }
 
 int main(int argc, char** argv) {
@@ -102,34 +153,30 @@ int main(int argc, char** argv) {
     eng.init();
 
     eng.test();
+
+    FreeImage_Initialise();
+
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
-    glutCreateWindow("freeglut + FreeImage");
-    std::printf("GL: %s | FreeImage: %s\n", (const char*)glGetString(GL_VERSION),
+    glutInitWindowSize(640, 480);
+    glutCreateWindow("freeglut + FreeImage test");
+
+    std::printf("GL_VERSION:  %s\nGL_RENDERER: %s\nFreeImage:   %s\n",
+                (const char*)glGetString(GL_VERSION), (const char*)glGetString(GL_RENDERER),
                 FreeImage_GetVersion());
 
-    // FreeImage: build a 64x64 checkerboard (32-bit, stored as BGRA)
-    FIBITMAP* img = FreeImage_Allocate(64, 64, 32);
-    for (unsigned y = 0; y < 64; ++y)
-        for (unsigned x = 0; x < 64; ++x) {
-            BYTE v = ((x / 8 + y / 8) % 2) ? 255 : 40;
-            RGBQUAD c = {v, 0, (BYTE)(255 - v), 255}; // B, G, R, A
-            FreeImage_SetPixelColor(img, x, y, &c);
-        }
-
-    // OpenGL: upload it as a texture
-    GLuint tex;
-    glGenTextures(1, &tex);
-    glBindTexture(GL_TEXTURE_2D, tex);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, 64, 64, 0, GL_BGRA, GL_UNSIGNED_BYTE,
-                 FreeImage_GetBits(img));
-    FreeImage_Unload(img);
-    glEnable(GL_TEXTURE_2D);
+    glClearColor(0.15f, 0.15f, 0.15f, 1.0f);
+    makeTexture();
 
     glutDisplayFunc(display);
+    glutReshapeFunc(reshape);
+    glutIdleFunc(idle);
+
+    // Return from glutMainLoop() when the window is closed, so cleanup runs
+    glutSetOption(GLUT_ACTION_ON_WINDOW_CLOSE, GLUT_ACTION_CONTINUE_EXECUTION);
     glutMainLoop();
+
+    FreeImage_DeInitialise();
 
     // Release engine:
     eng.free();
