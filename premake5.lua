0b5e8f424e4ee6729acef2b26799b4d7efc6bdc3
i <ismael.trentin@student.supsi.ch>
2026-09-22 20:08:07 +0200

    chore(premake): fix filtering order

--- premake5.lua
+++ premake5.lua
@@ -2,10 +2,36 @@ if os.isdir '.conan2/deps' then
     include '.conan2/deps/conandeps.premake5.lua'
 end
 
+---- WORKSPACE ----
 workspace 'cg-prj'
 configurations { 'Debug', 'Release' }
 architecture(os.hostarch())
 
+-- CONFIGS --
+filter { 'configurations:Debug' }
+defines { 'DEBUG' }
+symbols 'On'
+filter {}
+
+filter { 'configurations:Release' }
+defines { 'NDEBUG' }
+optimize 'On'
+filter {}
+
+-- DEFINITIONS --
+filter 'system:windows'
+defines { '_WINDOWS' }
+filter {}
+
+filter 'system:linux'
+defines { '_UNIX' }
+filter {}
+
+filter 'system:macosx'
+defines { '_APPLE' }
+filter {}
+
+---- PROJECTS ----
 -- ENGINE --
 project 'engine'
 location './engine'
@@ -19,10 +45,14 @@ includedirs { '%{prj.location}/include' }
 kind 'SharedLib'
 language 'C++'
 cppdialect 'C++20'
+buildoptions { '-std=c++20' }
 -- ENGINE /windows --
 filter 'system:windows'
 defines { '_WINDOWS', 'GRAPHICS_ENGINE_EXPORTS' }
 filter {}
+if os.isdir '.conan2/deps' then
+    conan_setup()
+end
 
 -- CLIENT --
 project 'client'
@@ -41,39 +71,15 @@ links { 'engine' }
 kind 'ConsoleApp'
 language 'C++'
 cppdialect 'C++20'
--- CLIENT /windows --
-filter 'system:windows'
-defines { '_WINDOWS' }
-filter {}
--- CLIENT /linux --
-filter 'system:linux'
--- links { 'GL', 'glut' }
-filter {}
--- CLIENT /macosx --
-filter 'system:macosx'
--- links { 'OpenGL.framework', 'glut' }
-filter {}
-
--- CONFIGS --
-filter { 'configurations:Debug' }
-defines { 'DEBUG' }
-symbols 'On'
-filter {}
-
-filter { 'configurations:Release' }
-defines { 'NDEBUG' }
-optimize 'On'
-filter {}
-
+buildoptions { '-std=c++20' }
 if os.isdir '.conan2/deps' then
     conan_setup()
 end
 
 -- CUSTOM OPTIONS --
-include 'lua/options/log.lua'
 include 'lua/options/all.lua'
 
 -- CUSTOM ACTIONS --
 include 'lua/actions/install.lua'
-include 'lua/actions/ecc_link.lua'
 include 'lua/actions/clean.lua'
+include 'lua/actions/docs.lua'
