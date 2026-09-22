cebccb1cc4112ac5aaf573eebc4d710d399b32d6
IsmaelTrentin <ismaeltrentin@gmail.com>
2026-09-22 22:59:24 +0200

    test: improve test case

--- test/src/engine.test.cpp
+++ test/src/engine.test.cpp
@@ -4,13 +4,23 @@
 
 TEST(EngineTest, GetInstanceReturnsValidRef) {
     Eng::Base& inst = Eng::Base::getInstance();
+    inst.init();
 
     SUCCEED();
+
+    inst.free();
 }
 
 TEST(EngineTest, EngineIsSingleton) {
     Eng::Base& inst1 = Eng::Base::getInstance();
     Eng::Base& inst2 = Eng::Base::getInstance();
 
+    EXPECT_EQ(&inst1, &inst2);
+
+
+    inst1.init();
+    EXPECT_EQ(&inst1, &inst2);
+
+    inst1.free();
     EXPECT_EQ(&inst1, &inst2);
 }
