c8379ec7fd607e4c7476b96814998c9c36042390
i <pandux66@gmail.com>
2026-09-22 23:27:05 +0200

    test(engine): add test case

--- test/src/engine.test.cpp
+++ test/src/engine.test.cpp
@@ -2,21 +2,27 @@
 
 #include <gtest/gtest.h>
 
-TEST(EngineTest, GetInstanceReturnsValidRef) {
+TEST(EngineTest, GetInstanceShouldReturnValidRef) {
     Eng::Base& inst = Eng::Base::getInstance();
-    inst.init();
 
+    SUCCEED();
+}
+
+TEST(EngineTest, ShouldInitAndFree) {
+    Eng::Base& inst = Eng::Base::getInstance();
+
+    inst.init();
     SUCCEED();
 
     inst.free();
+    SUCCEED();
 }
 
-TEST(EngineTest, EngineIsSingleton) {
+TEST(EngineTest, ShouldBeSingleton) {
     Eng::Base& inst1 = Eng::Base::getInstance();
     Eng::Base& inst2 = Eng::Base::getInstance();
 
     EXPECT_EQ(&inst1, &inst2);
-
 
     inst1.init();
     EXPECT_EQ(&inst1, &inst2);
